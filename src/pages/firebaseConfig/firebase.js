19865ce8416eb12868a3c2d549991b9950275d80
Kwabena <kasumadu@tapnhop.com>
2023-09-17 08:44:08 -0800

    add firebase again

--- src/pages/firebaseConfig/firebase.js
+++ src/pages/firebaseConfig/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey:  process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyCuXxCASqoK4NLnmr7J78HIwEZC9CHMk2U",
+  authDomain: "codrobot-e3b0d.firebaseapp.com",
+  databaseURL: "http://codrobot-e3b0d-default-rtdb.firebaseio.com/",
+  projectId: "codrobot-e3b0d",
+  storageBucket: "codrobot-e3b0d.appspot.com",
+  messagingSenderId: "48800254764",
+  appId: "1:48800254764:web:9c85cec4365a06acfa7a89",
+  measurementId: "G-6JQSE7S7KK"
 };
 
 const app = initializeApp(firebaseConfig);
