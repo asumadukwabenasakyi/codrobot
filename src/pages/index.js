0d066b9b9cabbf562170500aec8c4656c16aa6c9
Kwabena <kasumadu@tapnhop.com>
2023-09-17 07:58:42 -0800

    changed the title of the page and the icon of the url which appears at the title bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import styles from '../styles/Home.module.css';
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 function Index() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/codLogo.jpg" />
+        <title>Welcome To Cod Robot</title>
+      </Head>
       <div>This is the home page of the web application</div>
     </>
-  )
+  );
 }
 
 export default Index;
