7cf62cc69070caaaa5cd3cea596841ec43cd2ccd
Kwabena <kasumadu@tapnhop.com>
2023-09-17 07:51:13 -0800

    adding images and firabse config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Home.module.css';
 
 function Index() {
   return (
@@ -8,4 +9,4 @@ function Index() {
   )
 }
 
-export default Index
+export default Index;
